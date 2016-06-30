Replace geopy distance calculation with pyproj. Fixes issue #34
pyproj is needed anyway for basemap, so one dependency less.

mslib/mss_util.py
@@ -35,9 +35,26 @@ import numpy as np
 from scipy.interpolate import RectBivariateSpline, interp1d
 from scipy.ndimage import map_coordinates
 from mslib import greatcircle
-from geopy import distance
+try:
+    import mpl_toolkits.basemap.pyproj as pyproj
+except ImportError:
+    import pyproj
 
 # local application imports
+
+def get_distance(coord0, coord1):
+    """
+    Computes the distance between two points on the Earth surface
+    Args:
+        coord0: coordinate(lon/lat) of first point
+        coord1: coordinate(lon/lat) of second point
+
+    Returns:
+        length of distance in km
+    """
+    pr = pyproj.Geod(ellps='WGS84')
+    return pr.inv(coord0[1], coord0[0], coord1[1], coord1[0])[-1] / 1000.
+
 
 """
 Tangent point / Hexagon / Solar Angle utilities
@@ -408,7 +425,7 @@ def path_points(points, numpoints=100, connection='linear'):
                         (points[i][LON] - points[i + 1][LON]) ** 2)
         elif connection == 'greatcircle':
             # Use Vincenty distance provided by the geopy module.
-            d = distance.distance(points[i], points[i + 1]).km
+            d = get_distance(points[i], points[i + 1])
         distances.append(d)
     distances = np.array(distances)
 