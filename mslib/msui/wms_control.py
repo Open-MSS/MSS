297990c8b52100bdff1bdcee2ae526dc370cc1c1
Joern Ungermann <j.ungermann@fz-juelich.de>
2016-11-18 20:52:29 +0100

    Fixes issue #73
    Strangely, the title text was updated correctly in Matplotlib, and also
    commanded to be redrawn, but only following a system-event like a mouse
    click opening another widget or a switch to another application, the
    text was actually updated. Adding a QT repaint command solves the issue.
    Maybe a bug in QT.

--- mslib/msui/wms_control.py
+++ mslib/msui/wms_control.py
@@ -1597,12 +1597,12 @@ class HSecWMSControlWidget(WMSControlWidget):
                 tph = self.tp_height
                 if tph is not None:
                     tph = "{:.1f} km".format(tph)
-                self.view.drawMetadata(title=self.get_layer_object(layer).title,
-                                       init_time=init_time,
-                                       valid_time=valid_time,
-                                       level=level,
-                                       style=style_title,
-                                       tp=tph)
+                #self.view.drawMetadata(title=self.get_layer_object(layer).title,
+                #                       init_time=init_time,
+                #                       valid_time=valid_time,
+                #                       level=level,
+                #                       style=style_title,
+                #                       tp=tph)
                 self.view.waypoints_interactor.update()
 
     def set_tp_height(self, height=None):
@@ -1627,7 +1627,6 @@ class HSecWMSControlWidget(WMSControlWidget):
             logging.error("an error occurred. no image retried. (%s)" % ex)
         else:
             # Plot the image on the view canvas.
-            self.view.drawImage(img)
             if style != "":
                 style_title = self.get_layer_object(layer).styles[style]["title"]
             else:
@@ -1641,6 +1640,7 @@ class HSecWMSControlWidget(WMSControlWidget):
                                    level=level,
                                    style=style_title,
                                    tp=tph)
+            self.view.drawImage(img)
             self.view.drawLegend(legend_img)
             self.view.waypoints_interactor.update()
 
