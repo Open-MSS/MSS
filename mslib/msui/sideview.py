Open-MSS/MSS

commit 7929ba2f25b9c077af8c51694e0a530a6501d0c4

fixes of pylint and PEP8 errors in msui.

mslib/msui/sideview.py
@@ -54,9 +54,9 @@ from mslib.msui import constants
 # Dock window indices.
 WMS = 0
 
-"""
-DIALOG for setting sideview options
-"""
+#
+# DIALOG for setting sideview options
+#
 
 
 class MSS_SV_OptionsDialog(QtGui.QDialog, ui_opt.Ui_SideViewOptionsDialog):
@@ -201,9 +201,9 @@ class MSS_SV_OptionsDialog(QtGui.QDialog, ui_opt.Ui_SideViewOptionsDialog):
         return settings_dict
 
 
-"""
-CLASS MSSSideViewWindow
-"""
+#
+# CLASS MSSSideViewWindow
+#
 
 
 class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
@@ -283,7 +283,7 @@ class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
     def setOptions(self):
         """Slot to open a dialog that lets the user specifiy sideview options.
         """
-        settings = self.getView().getSettings()
+        settings = self.getView().get_settings()
         dlg = MSS_SV_OptionsDialog(parent=self, settings_dict=settings)
         dlg.setModal(True)
         if dlg.exec_() == QtGui.QDialog.Accepted:
@@ -298,8 +298,8 @@ class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
         """
         # TODO: ConfigParser and a central configuration file might be the better solution than pickle.
         # http://stackoverflow.com/questions/200599/whats-the-best-way-to-store-simple-user-settings-in-python
-        settings = self.getView().getSettings()
-        logging.debug("storing settings to %s" % self.settingsfile)
+        settings = self.getView().get_settings()
+        logging.debug("storing settings to %s", self.settingsfile)
         fileobj = open(self.settingsfile, "w")
         pickle.dump(settings, fileobj)
         fileobj.close()
@@ -308,14 +308,14 @@ class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
         """Load settings from the file self.settingsfile.
         """
         if os.path.exists(self.settingsfile):
-            logging.debug("loading settings from %s" % self.settingsfile)
+            logging.debug("loading settings from %s", self.settingsfile)
             fileobj = open(self.settingsfile, "r")
             settings = pickle.load(fileobj)
             fileobj.close()
             self.getView().setSettings(settings)
 
 
-if __name__ == "__main__":
+def _main():
     # Log everything, and send it to stderr.
     # See http://docs.python.org/library/logging.html for more information
     # on the Python logging module.
@@ -335,7 +335,10 @@ if __name__ == "__main__":
     waypoints_model.insertRows(0, rows=len(initial_waypoints),
                                waypoints=initial_waypoints)
 
-    app = QtGui.QApplication(sys.argv)
-    win = MSSSideViewWindow(model=waypoints_model)
-    win.show()
-    sys.exit(app.exec_())
+    application = QtGui.QApplication(sys.argv)
+    window = MSSSideViewWindow(model=waypoints_model)
+    window.show()
+    sys.exit(application.exec_())
+
+if __name__ == "__main__":
+    _main()

mslib/_tests/test_netCDF4tools.py
@@ -38,8 +38,7 @@ class Test_netCDF4tools():
                      ('longitude', u'lon'),
                      ('atmosphere_pressure_coordinate', 'hyam'),
                      ('atmosphere_hybrid_height_coordinate', 'hybm'),
-                     ('cloud_area_fraction_in_atmosphere_layer', 'Fraction_of_cloud_cover_hybrid')
-                    ]
+                     ('cloud_area_fraction_in_atmosphere_layer', 'Fraction_of_cloud_cover_hybrid')]
         for standard_name, short_name in checklist:
             variable = identify_variable(self.ncfile_ml, standard_name)
             assert variable[0] == short_name

mslib/msui/aircrafts.py
@@ -80,7 +80,7 @@ class SimpleAircraft(object):
         idx0, w0, idx1, w1 = self._get_weights(weights, grossweight)
         return self._interpolate_alt(data[idx0], altitude) * w0 + self._interpolate_alt(data[idx1], altitude) * w1
 
-    def climbPerformance(self, altitude, grossweight):
+    def get_climb_performance(self, altitude, grossweight):
         """
         Climb performance of the aircraft. Returns time [min], distance [nm] and
         fuel [lbs] required for a climb from sea level to the specified
@@ -92,7 +92,7 @@ class SimpleAircraft(object):
         """
         return self._interpolate(self._climb, altitude, grossweight)
 
-    def cruisePerformance(self, altitude, grossweight):
+    def get_cruise_performance(self, altitude, grossweight):
         """
         Cruise performance of the aircraft. Returns true airspeed [knots] and
         fuelflow [lbs/hr] required for a cruise at the specified altitude.
@@ -103,7 +103,7 @@ class SimpleAircraft(object):
         """
         return self._interpolate(self._cruise, altitude, grossweight)
 
-    def descentPerformance(self, altitude, grossweight):
+    def get_descent_performance(self, altitude, grossweight):
         """
         Descent performance of the aircraft. Returns time [min], distance [nm]
         and fuel [lbs] required for a climb from sea level to the specified

mslib/msui/constants.py
@@ -36,7 +36,7 @@ CONFIG_PATH = os.path.join(HOME, ".config")
 MSS_CONFIG_PATH = os.getenv("MSS_CONFIG_PATH", os.path.join(HOME, ".config", "mss"))
 if not os.path.exists(MSS_CONFIG_PATH):
     os.makedirs(MSS_CONFIG_PATH)
-    
+
 MSS_SETTINGS = os.getenv('MSS_SETTINGS', os.path.join(MSS_CONFIG_PATH, "mss_settings.json"))
 
 WMS_LOGIN_CACHE = {}

mslib/msui/flighttrack.py
@@ -61,15 +61,14 @@ from mslib.msui.performance_settings import DEFAULT_PERFORMANCE
 from mslib.msui import MissionSupportSystemDefaultConfig as mss_default
 
 
-"""
-CONSTANTS (used in this module)
-"""
+# CONSTANTS (used in this module)
+#
 # Constants for identifying the table columns when the WaypointsTableModel is
 # used with a QTableWidget.
 LOCATION, LAT, LON, FLIGHTLEVEL, PRESSURE = range(5)
 
 
-def secToStr(seconds):
+def seconds_to_string(seconds):
     """Format a time given in seconds to a string HH:MM:SS. Used for the
        'leg time/cum. time' columns of the table view.
     """
@@ -88,8 +87,8 @@ TABLE_FULL = [
                                                            waypoint.distance_to_prev / 1.852), False),
     ("Cum. dist.\n(km [nm])", lambda waypoint: "%d [%d]" % (waypoint.distance_total,
                                                             waypoint.distance_total / 1.852), False),
-    ("Leg time", lambda waypoint: secToStr(waypoint.leg_time), False),
-    ("Cum. time", lambda waypoint: secToStr(waypoint.cum_time), False),
+    ("Leg time", lambda waypoint: seconds_to_string(waypoint.leg_time), False),
+    ("Cum. time", lambda waypoint: seconds_to_string(waypoint.cum_time), False),
     ("Time (UTC)", lambda waypoint: waypoint.utc_time.strftime("%Y-%m-%d %H:%M:%S"), False),
     ("Rem. fuel\n(lb)", lambda waypoint: ("%d" % waypoint.rem_fuel), False),
     ("Aircraft\nweight (lb)", lambda waypoint: ("%d" % waypoint.weight), False),
@@ -119,9 +118,9 @@ class Waypoint(object):
             self.lon = lon
         self.flightlevel = flightlevel
         self.pressure = thermolib.flightlevel2pressure(flightlevel)
-        self.comments = comments
         self.distance_to_prev = 0.
         self.distance_total = 0.
+        self._comments = comments
 
         # Performance fields (for values read from the flight performance
         # service).
@@ -139,7 +138,7 @@ class Waypoint(object):
         return self._comments
 
     def set_comments(self, string):
-        if type(string) is str:
+        if isinstance(string, basestring):
             self._comments = QString(string)
         else:
             self._comments = string
@@ -182,7 +181,7 @@ class WaypointsTableModel(QAbstractTableModel):
             if filename.endswith(".ftml"):
                 self.loadFromFTML(filename)
             else:
-                logging.debug("No known file extension! {:}".format(filename))
+                logging.debug("No known file extension! %s", filename)
 
         if waypoints:
             self.replaceWaypoints(waypoints)
@@ -191,7 +190,7 @@ class WaypointsTableModel(QAbstractTableModel):
         """Load settings from the file self.settingsfile.
         """
         if os.path.exists(self.settingsfile):
-            logging.debug("loading settings from %s" % self.settingsfile)
+            logging.debug("loading settings from %s", self.settingsfile)
             with open(self.settingsfile, "r") as fileobj:
                 self.performance_settings = pickle.load(fileobj)
         else:
@@ -203,7 +202,7 @@ class WaypointsTableModel(QAbstractTableModel):
         """
         # TODO: ConfigParser and a central configuration file might be the better solution than pickle.
         # http://stackoverflow.com/questions/200599/whats-the-best-way-to-store-simple-user-settings-in-python
-        logging.debug("storing settings to %s" % self.settingsfile)
+        logging.debug("storing settings to %s", self.settingsfile)
         with open(self.settingsfile, "w") as fileobj:
             pickle.dump(self.performance_settings, fileobj)
 
@@ -242,8 +241,7 @@ class WaypointsTableModel(QAbstractTableModel):
         """
         waypoints = self.waypoints
 
-        if not index.isValid() or \
-                not (0 <= index.row() < len(waypoints)):
+        if not index.isValid() or not (0 <= index.row() < len(waypoints)):
             return QVariant()
         waypoint = waypoints[index.row()]
         column = index.column()
@@ -435,17 +433,17 @@ class WaypointsTableModel(QAbstractTableModel):
         def get_duration_fuel(flightlevel0, flightlevel1, distance, weight):
             aircraft = self.performance_settings["aircraft"]
             if flightlevel0 == flightlevel1:
-                tas, fuelflow = aircraft.cruisePerformance(flightlevel0 * 100, weight)
+                tas, fuelflow = aircraft.get_cruise_performance(flightlevel0 * 100, weight)
                 duration = 3600. * distance / (1.852 * tas)  # convert to s (tas is in nm/h)
                 leg_fuel = duration * fuelflow / 3600.
                 return duration, leg_fuel
             else:
                 if flightlevel0 < flightlevel1:
-                    duration0, dist0, fuel0 = aircraft.climbPerformance(flightlevel0 * 100, weight)
-                    duration1, dist1, fuel1 = aircraft.climbPerformance(flightlevel1 * 100, weight)
+                    duration0, dist0, fuel0 = aircraft.get_climb_performance(flightlevel0 * 100, weight)
+                    duration1, dist1, fuel1 = aircraft.get_climb_performance(flightlevel1 * 100, weight)
                 else:
-                    duration0, dist0, fuel0 = aircraft.descentPerformance(flightlevel0 * 100, weight)
-                    duration1, dist1, fuel1 = aircraft.descentPerformance(flightlevel1 * 100, weight)
+                    duration0, dist0, fuel0 = aircraft.get_descent_performance(flightlevel0 * 100, weight)
+                    duration1, dist1, fuel1 = aircraft.get_descent_performance(flightlevel1 * 100, weight)
                 duration = (duration1 - duration0) * 60  # convert from min to s
                 dist = (dist1 - dist0) * 1.852  # convert from nm to km
                 fuel = fuel1 - fuel0
@@ -592,15 +590,15 @@ class WaypointsTableModel(QAbstractTableModel):
             waypoints_list.append(Waypoint(lat, lon, flightlevel,
                                            location=location,
                                            comments=comments))
-        self.replaceWaypoints(self, waypoints_list)
+        self.replaceWaypoints(waypoints_list)
 
     def getFilename(self):
         return self.filename
 
 
-"""
-CLASS  WaypointDelegate
-"""
+#
+# CLASS  WaypointDelegate
+#
 
 
 class WaypointDelegate(QItemDelegate):

mslib/msui/hexagon_dockwidget.py
@@ -59,25 +59,24 @@ class HexagonControlWidget(QtGui.QWidget, ui.Ui_HexagonDockWidget):
         params = self._get_parameters()
 
         if params["radius"] < 0.01:
-            QtGui.QMessageBox.warning(self, "Add hexagon",
-                "You cannot create a hexagon with zero radius!",
+            QtGui.QMessageBox.warning(
+                self, "Add hexagon", "You cannot create a hexagon with zero radius!",
                 QtGui.QMessageBox.Ok)
             return
         points = create_hexagon(params["center_lat"], params["center_lon"], params["radius"], params["angle"])
         index = table_view.currentIndex()
         if not index.isValid():
             row = 0
-            fl = config_loader(dataset="new_flighttrack_flightlevel",
-                               default=mss_default.new_flighttrack_flightlevel)
+            flightlevel = config_loader(dataset="new_flighttrack_flightlevel",
+                                        default=mss_default.new_flighttrack_flightlevel)
         else:
             row = index.row() + 1
-            fl = waypoints_model.waypointData(row - 1).flightlevel
+            flightlevel = waypoints_model.waypointData(row - 1).flightlevel
         waypoints = []
-        for i, p in enumerate(points):
-            waypoints.append(ft.Waypoint(lon=float(round(p[1], 2)),
-                                         lat=float(round(p[0], 2)),
-                                         flightlevel=float(fl),
-                                         comments="Hexagon {:d}".format(i + 1)))
+        for i, point in enumerate(points):
+            waypoints.append(
+                ft.Waypoint(lon=float(round(point[1], 2)), lat=float(round(point[0], 2)),
+                            flightlevel=float(flightlevel), comments="Hexagon {:d}".format(i + 1)))
         waypoints_model.insertRows(row, rows=len(waypoints), waypoints=waypoints)
         index = waypoints_model.index(row, 0)
         table_view.setCurrentIndex(index)
@@ -107,7 +106,7 @@ class HexagonControlWidget(QtGui.QWidget, ui.Ui_HexagonDockWidget):
                 else:
                     found_one = False
                     for i in range(0, row_max - row_min):
-                        if str(waypoints_model.waypointData(row_min + i).comments) != "Hexagon {:d}".format(i+1):
+                        if str(waypoints_model.waypointData(row_min + i).comments) != "Hexagon {:d}".format(i + 1):
                             found_one = True
                             break
                     if found_one:
@@ -117,11 +116,11 @@ class HexagonControlWidget(QtGui.QWidget, ui.Ui_HexagonDockWidget):
                         sel = QtGui.QMessageBox.question(
                             None, "Remove hexagon",
                             "This will remove waypoints {:d}-{:d}. Continue?".format(row_min, row_max),
-                            QtGui.QMessageBox.Yes|QtGui.QMessageBox.No)
+                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
                         if sel == QtGui.QMessageBox.Yes:
                             waypoints_model.removeRows(row_min, rows=7)
             else:
                 raise HexagonException("Cannot remove hexagon, please select a hexagon "
                                        "waypoint ('Hexagon x' in comments field)")
-        except HexagonException, e:
-            QtGui.QMessageBox.warning(self  , "Remove hexagon", str(e), QtGui.QMessageBox.Ok)
+        except HexagonException, ex:
+            QtGui.QMessageBox.warning(self, "Remove hexagon", str(ex), QtGui.QMessageBox.Ok)

mslib/msui/kmloverlay_dockwidget.py
@@ -19,6 +19,10 @@ import pykml.parser
 
 
 class KMLOverlayControlWidget(QtGui.QWidget, ui.Ui_KMLOverlayDockWidget):
+    """
+    This class provides the interface for accessing KML files and
+    adding the appropriate patches to the TopView canvas.
+    """
     def __init__(self, parent=None, view=None):
         super(KMLOverlayControlWidget, self).__init__(parent)
         self.setupUi(self)
@@ -39,6 +43,10 @@ class KMLOverlayControlWidget(QtGui.QWidget, ui.Ui_KMLOverlayDockWidget):
         self.cbOverlay.setEnabled(False)
 
     def update_settings(self):
+        """
+        Called when the visibility checkbox is toggled and hides/shows
+        the overlay if loaded.
+        """
         if self.view and self.cbOverlay.isChecked() and self.patch:
             self.view.kmloverlay = self.patch
             self.patch.update()
@@ -59,6 +67,10 @@ class KMLOverlayControlWidget(QtGui.QWidget, ui.Ui_KMLOverlayDockWidget):
         self.leFile.setText(fname)
 
     def load_file(self):
+        """
+        Loads an KML file selected by the leFile box and constructs the
+        corresponding patch.
+        """
         if self.patch:
             self.patch.remove()
             self.view.kmloverlay = None

mslib/msui/lagranto_output_reader.py
@@ -90,9 +90,9 @@ import numpy
 # local application imports
 
 
-"""
-CLASS LagrantoOutputReader
-"""
+#
+# CLASS LagrantoOutputReader
+#
 
 
 class LagrantoOutputReader(object):

commit 4a859d829d0fd18f349c9a2d50ec056ac8db3fc6

Fixed bug introduced in 7929ba2.

mslib/msui/sideview.py
@@ -283,7 +283,7 @@ class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
     def setOptions(self):
         """Slot to open a dialog that lets the user specifiy sideview options.
         """
-        settings = self.getView().get_settings()
+        settings = self.getView().getSettings()
         dlg = MSS_SV_OptionsDialog(parent=self, settings_dict=settings)
         dlg.setModal(True)
         if dlg.exec_() == QtGui.QDialog.Accepted:
@@ -298,7 +298,7 @@ class MSSSideViewWindow(mss_qt.MSSMplViewWindow, ui.Ui_SideViewWindow):
         """
         # TODO: ConfigParser and a central configuration file might be the better solution than pickle.
         # http://stackoverflow.com/questions/200599/whats-the-best-way-to-store-simple-user-settings-in-python
-        settings = self.getView().get_settings()
+        settings = self.getView().getSettings()
         logging.debug("storing settings to %s", self.settingsfile)
         fileobj = open(self.settingsfile, "w")
         pickle.dump(settings, fileobj)