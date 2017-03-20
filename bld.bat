e9edfbd0860e54af035220d811530b15c2903765
Joern Ungermann <j.ungermann@fz-juelich.de>
2017-03-20 18:16:19 +0100

    Added icon installation on Windows

--- bld.bat
+++ bld.bat
@@ -1,2 +1,9 @@
-"%PYTHON%" setup.py install
+set MENU_DIR=%PREFIX%\Menu
+if not exist %MENU_DIR% mkdir %MENU_DIR%
+if errorlevel 1 exit 1
+copy %SRC_DIR%\Menu\mss_shortcut.json %MENU_DIR%\mss_shortcut.json
+if errorlevel 1 exit 1
+copy %SRC_DIR%\Menu\mss.ico %MENU_DIR%\mss.ico
+if errorlevel 1 exit 1
+%PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
