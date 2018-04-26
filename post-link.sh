fb0b1a940348e8d8af83b172271651671c00a630
Reimar Bauer <rb.proj@gmail.com>
2018-04-26 09:48:42 +0200

    Docker setup updated to python 3, fixes #207, #333

--- post-link.sh
+++ post-link.sh
@@ -1,4 +1,7 @@
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
+# only if we don't execute in a docker environment
+if [[ "$(cat /proc/1/sched | head -n 1)" = "systemd (1, #threads: 1)" ]] && [[ "$unamestr" == 'Linux' ]]; then
     mss -m
+else 
+	echo "called in a container"
 fi
