30eb50d065d38882b0b4c385c46e23fc0eabdbe3
Shivashis Padhi <shivashispadhi@gmail.com>
2019-06-07 17:55:29 +0530

    Add user to room when connected, define Projects, refactor app to avoid cyclic dependencies

--- mslib/mscolab/sockets_manager.py
+++ mslib/mscolab/sockets_manager.py
@@ -1,16 +1,17 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, rooms
 from flask import request
 import logging
 
-from models import Permission, User
+from models import Permission, User, Project
+
+socketio = SocketIO()
 
 
 class SocketsManager(object):
 	"""Class with handler functions for socket related"""
 
 	def __init__(self):
-		super(SocketsManager, self).__init__()		
-		self.rooms = []
+		super(SocketsManager, self).__init__()
 		self.sockets = []
 
 
@@ -32,15 +33,36 @@ class SocketsManager(object):
 		# search and add user to respective rooms
 		for permission in permissions:
 			print(permission.p_id, permission.u_id)
-
+			# for each project with p_id, search rooms
+			# socketio.join_room(room, sid=None, namespace=None)
+			"""
+			- a client is always registered as a room with name equal to 
+			the session id of the client.
+			- so the rooms can safely be named as stringified versions of 
+			the project id.
+			- thus, an abstraction is unnecessary. if it will be, it'll be
+			considered during later developments.
+			- so joining the actual socketio room would be enough
+			"""
+			join_room(str(permission.p_id))
+			print(rooms())
 		socket_storage = {
-			'id': request.sid,
-			'emailid': user.emailid
+			's_id': request.sid,
+			'u_id': user.id
 		}
 		self.sockets.append(socket_storage)
+
 
 	def handle_disconnect(self):
 		logging.info("disconnected")
 		logging.info(request.sid)
 		# remove socket from socket_storage
-		self.sockets[:] = [d for d in self.sockets if d['id'] != request.sid]
+		self.sockets[:] = [d for d in self.sockets if d['s_id'] != request.sid]
+
+
+sm = SocketsManager()
+
+# sockets related handlers
+socketio.on_event('connect', sm.handle_connect)
+socketio.on_event('start_event', sm.handle_start_event)
+socketio.on_event('disconnect', sm.handle_disconnect)
