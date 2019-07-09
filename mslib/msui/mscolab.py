06d18677fe8bcc6e6de47cfd735a1d219a94db0c
Shivashis Padhi <shivashispadhi@gmail.com>
2019-07-09 16:09:43 +0530

    add sclient socket handlers, file-save functionality

--- mslib/msui/mscolab.py
+++ mslib/msui/mscolab.py
@@ -35,6 +35,7 @@ from mslib.msui import MissionSupportSystemDefaultConfig as mss_default
 from mslib.msui.icons import icons
 from mslib.msui import flighttrack as ft
 from mslib.msui import topview, sideview, tableview
+from mslib.msui import socket_control as sc
 
 import logging
 import requests
@@ -71,6 +72,10 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
         self.waypoints_model = None
         # store a reference of window in class
         self.open_windows_mscolab = []
+        # connection object to interact with sockets
+        self.conn = None
+        # to store tempfile name
+        self.fname_temp = ""
 
     def authorize(self):
         emailid = self.emailid.text()
@@ -102,6 +107,9 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
             projects = json_["projects"]
             self.add_projects_to_ui(projects)
 
+            # create socket connection here
+            self.conn = sc.ConnectionManager(self.token)
+
     def add_projects_to_ui(self, projects):
         for project in projects:
             project_desc = project['path'] + " (" + project["access_level"] + ")"
@@ -124,6 +132,7 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
         data_dir = fs.open_fs(mss_default.data_dir)
         data_dir.writetext('tempfile_mscolab.ftml', ftml)
         fname_temp = fs.path.combine(mss_default.data_dir, 'tempfile_mscolab.ftml')
+        self.fname_temp = fname_temp
         self.waypoints_model = ft.WaypointsTableModel(filename=fname_temp)
 
         # change font style for selected
@@ -160,13 +169,27 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
     def logout(self):
         # delete token and show login widget-items
         self.token = None
+        # delete active-project-id
+        self.active_pid = None
         # clear projects list here
         self.widget_2.hide()
         self.widget.show()
+        # clear project listing
+        self.listProjects.clear()
+        # disconnect socket
+        if self.conn != None:
+            self.conn.disconnect()
+            self.conn = None
 
     def save_wp_mscolab(self):
         if self.active_pid != None:
-            self.waypoints_model.save_to_mscolab()
+            # to save to temp file
+            xml_text = self.waypoints_model.save_to_mscolab()
+            # to emit to mscolab
+            self.conn.save_file(self.token, self.active_pid, xml_text)
 
     def setIdentifier(self, identifier):
         self.identifier = identifier
+
+    def closeEvent(self, event):
+        self.conn.disconnect()
