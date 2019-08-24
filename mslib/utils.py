38a4c92ce7e759fcff83f66548a338365ad75271
Anveshan Lal <anveshanrx8@gmail.com>
2019-08-24 18:57:00 +0530

    Flake8 errors resolved

--- mslib/utils.py
+++ mslib/utils.py
@@ -318,14 +318,14 @@ def get_projection_params(proj):
             proj4_params = ccrs.epsg(epsg).proj4_params
             lat_0, lon_0, lat_ts = proj4_params['lat_0'], proj4_params['lon_0'], proj4_params['lat_ts']
             proj_params = {"basemap": {"projection": ccrs.Stereographic(
-                                                        central_latitude=lat_0, central_longitude=lon_0, true_scale_latitude=lat_ts)},
-                                                        "bbox": "meter", "fixed": True}
+                           central_latitude=lat_0, central_longitude=lon_0, true_scale_latitude=lat_ts)},
+                           "bbox": "meter", "fixed": True}
         elif epsg in ("3995", "3996", "32661"):
             proj4_params = ccrs.epsg(epsg).proj4_params
             lat_0, lon_0, lat_ts = proj4_params['lat_0'], proj4_params['lon_0'], proj4_params['lat_ts']
             proj_params = {"basemap": {"projection": ccrs.Stereographic(
-                                                        central_latitude=lat_0, central_longitude=lon_0, true_scale_latitude=lat_ts)},
-                                                        "bbox": "meter", "fixed": True}
+                           central_latitude=lat_0, central_longitude=lon_0, true_scale_latitude=lat_ts)},
+                           "bbox": "meter", "fixed": True}
         elif epsg in ("3411", "3413", "3575"):
             proj_params = {"basemap": {"projection": ccrs.epsg(epsg)}, "bbox": "meter", "fixed": True}
         elif epsg in ("3395", "3857"):
