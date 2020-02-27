7437950f266366000af9422eea12cdd32507157c
Shivashis Padhi <shivashispadhi@gmail.com>
2020-02-27 10:15:53 +0530

    SocketIO works with WSGI on setup

--- mslib/mscolab/server.py
+++ mslib/mscolab/server.py
@@ -48,6 +48,7 @@ log.setLevel(logging.ERROR)
 def initialize_managers(app):
     sockio, cm, fm = setup_managers(app)
     # initiatializing socketio and db
+    app.wsgi_app = socketio.Middleware(socketio.server, app.wsgi_app)
     sockio.init_app(app)
     db.init_app(app)
     return (app, sockio, cm, fm)
