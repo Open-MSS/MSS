96bacce77070ee93e0ee94017665ee96f468135f
Aravind Murali <aravindmurali711@gmail.com>
2021-03-14 12:56:33 +0530

    Fix for Issue #636 (#716)
    * replaced qt file picker with fs for import/export
    
    * fix for #636
    
    * added comment

--- mslib/msui/mscolab.py
+++ mslib/msui/mscolab.py
@@ -42,7 +42,7 @@ from mslib.msui import mscolab_project as mp
 from mslib.msui import mscolab_version_history as mvh
 from mslib.msui import sideview, tableview, topview
 from mslib.msui import socket_control as sc
-from mslib.msui.mss_qt import QtCore, QtGui, QtWidgets, get_open_filename
+from mslib.msui.mss_qt import QtCore, QtGui, QtWidgets, get_open_filename, get_save_filename
 from mslib.msui.qt5 import ui_mscolab_help_dialog as msc_help_dialog
 from mslib.msui.qt5 import ui_add_project_dialog as add_project_ui
 from mslib.msui.qt5 import ui_add_user_dialog as add_user_ui
@@ -214,8 +214,8 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
             show_popup(self, "Error", "Some unexpected error occurred. Please try again.")
 
     def handle_import(self):
-        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select a file", "", "Flight track (*.ftml)")
-        if file_path == "":
+        file_path = get_open_filename(self, "Select a file", "", "Flight track (*.ftml)")
+        if file_path is None:
             return
         dir_path, file_name = fs.path.split(file_path)
         with open_fs(dir_path) as file_dir:
@@ -236,9 +236,10 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
         show_popup(self, "Import Success", f"The file - {file_name}, was imported successfully!", 1)
 
     def handle_export(self):
-        file_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save Flight track", self.active_project_name,
-                                                             "Flight track (*.ftml)")
-        if file_path == "":
+        # Setting default filename path for filedialogue
+        default_filename = self.active_project_name + ".ftml"
+        file_path = get_save_filename(self, "Save Flight track", default_filename, "Flight track (*.ftml)")
+        if file_path is None:
             return
         xml_doc = self.waypoints_model.get_xml_doc()
         dir_path, file_name = fs.path.split(file_path)
