0b1164268dd82e6645f16dda5c6c262b6720ce9a
Jatin Jain <72596619+Jatin2020-24@users.noreply.github.com>
2021-04-06 16:13:11 +0530

    Update Copyright Year

--- mslib/mswms/demodata.py
+++ mslib/mswms/demodata.py
@@ -10,7 +10,7 @@
     This file is part of mss.
 
     :copyright: Copyright 2017 Jens-Uwe Grooss, Joern Ungermann, Reimar Bauer
-    :copyright: Copyright 2017-2020 by the mss team, see AUTHORS.
+    :copyright: Copyright 2017-2021 by the mss team, see AUTHORS.
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
