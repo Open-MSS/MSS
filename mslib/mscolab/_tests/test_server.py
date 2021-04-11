4aededfe04fe3279c5e08846b96a557bdd78d322
Reimar Bauer <rb.proj@gmail.com>
2021-04-11 11:48:14 +0200

    missing import added

--- mslib/mscolab/_tests/test_server.py
+++ mslib/mscolab/_tests/test_server.py
@@ -23,6 +23,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
+import pytest
 import io
 from pathlib import Path
 from flask import json
