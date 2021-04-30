2347527f3a5bad90656a74e20eb1981785ac1e02
May <baermay98@gmail.com>
2021-04-30 13:07:59 +0200

    Fix signal leak

--- mslib/msui/mscolab.py
+++ mslib/msui/mscolab.py
@@ -169,6 +169,7 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
         self.password.setEnabled(False)
         # toggle to connect button
         self.toggleConnectionBtn.setText('Connect')
+        self.toggleConnectionBtn.clicked.disconnect(self.disconnect_handler)
         self.toggleConnectionBtn.clicked.connect(self.connect_handler)
         self.url.setEnabled(True)
         # set mscolab_server_url to None
@@ -201,6 +202,7 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
                 self.password.textChanged[str].connect(self.text_changed)
                 # toggle to disconnect button
                 self.toggleConnectionBtn.setText('Disconnect')
+                self.toggleConnectionBtn.clicked.disconnect(self.connect_handler)
                 self.toggleConnectionBtn.clicked.connect(self.disconnect_handler)
                 self.url.setEnabled(False)
                 if self.mscolab_server_url not in self.settings["server_settings"].keys():
