4e411f1db41b64060544e13189898a593d205ba2
Jatin Jain <jatinalwar2001@gmail.com>
2021-05-28 19:12:47 +0530

    updated copyright year

--- mslib/msui/mscolab_admin_window.py
+++ mslib/msui/mscolab_admin_window.py
@@ -9,6 +9,7 @@
     This file is part of mss.
 
     :copyright: 2020 Tanish Grover
+    :copyright: Copyright 2020-2021 by the mss team, see AUTHORS.
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
