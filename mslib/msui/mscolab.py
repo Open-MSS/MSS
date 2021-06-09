fde1fa59ec56ff745fa2dde6048a4094dc9398e6
Aravind Murali <aravindmurali711@gmail.com>
2021-06-09 15:26:23 +0530

    fixed tableview not opening bug; added test

--- mslib/msui/mscolab.py
+++ mslib/msui/mscolab.py
@@ -966,7 +966,7 @@ class MSSMscolabWindow(QtWidgets.QMainWindow, ui.Ui_MSSMscolabWindow):
             view_window = linearview.MSSLinearViewWindow(model=self.waypoints_model,
                                                          parent=self.listProjects,
                                                          _id=self.id_count)
-            view_window.view_type = "tableview"
+            view_window.view_type = "linearview"
         if self.access_level == "viewer":
             self.disable_navbar_action_buttons(_type, view_window)
 
