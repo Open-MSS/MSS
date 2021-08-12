8cdbaa172a3348ffaa9f1bff24a7a3ef8006d282
ReimarBauer <rb.proj@gmail.com>
2021-08-12 12:31:53 +0200

    refactored test_files to test_client

--- mslib/mscolab/_tests/test_files.py
+++ mslib/mscolab/_tests/test_files.py
@@ -25,12 +25,13 @@
     limitations under the License.
 """
 # ToDo have to be merged into test_file_manager
-from flask import Flask
+from flask_testing import TestCase
 import os
 import pytest
 
 from mslib.mscolab.conf import mscolab_settings
 from mslib.mscolab.models import User, Project, Permission, Change, Message, db
+from mslib.mscolab.server import APP
 from mslib.mscolab.file_manager import FileManager
 from mslib.mscolab.seed import add_user, get_user
 from mslib.mscolab.mscolab import handle_db_seed
@@ -39,14 +40,22 @@ from mslib.mscolab.utils import get_recent_pid
 
 @pytest.mark.skipif(os.name == "nt",
                     reason="multiprocessing needs currently start_method fork")
-class Test_Files(object):
-    def setup(self):
+class Test_Files(TestCase):
+    render_templates = False
+
+    def create_app(self):
+        app = APP
+        app.config['SQLALCHEMY_DATABASE_URI'] = mscolab_settings.SQLALCHEMY_DB_URI
+        app.config['MSCOLAB_DATA_DIR'] = mscolab_settings.MSCOLAB_DATA_DIR
+        app.config['UPLOAD_FOLDER'] = mscolab_settings.UPLOAD_FOLDER
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config["TESTING"] = True
+        app.config['LIVESERVER_TIMEOUT'] = 10
+        app.config['LIVESERVER_PORT'] = 0
+        return app
+
+    def setUp(self):
         handle_db_seed()
-        self.app = Flask(__name__, static_url_path='')
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = mscolab_settings.SQLALCHEMY_DB_URI
-        self.app.config['MSCOLAB_DATA_DIR'] = mscolab_settings.MSCOLAB_DATA_DIR
-        self.app.config['UPLOAD_FOLDER'] = mscolab_settings.UPLOAD_FOLDER
-        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         db.init_app(self.app)
 
         self.fm = FileManager(self.app.config["MSCOLAB_DATA_DIR"])
@@ -62,11 +71,14 @@ class Test_Files(object):
         self.file_message_counter = [0] * 2
         self._example_data()
 
-    def teardown(self):
+    def tearDown(self):
         pass
+        # review later when handle_db does not seed for tests
+        # db.session.remove()
+        # db.drop_all()
 
     def test_create_project(self):
-        with self.app.app_context():
+        with self.app.test_client():
             # test for blank character in path
             assert self.fm.create_project('test path', 'test desc.', self.user) is False
             # test for normal path
@@ -84,7 +96,7 @@ class Test_Files(object):
             assert perms[0].u_id == self.user.id
 
     def test_projects(self):
-        with self.app.app_context():
+        with self.app.test_client():
             projects = self.fm.list_projects(self.user)
             assert len(projects) == 0
             assert self.fm.create_project('test_path', 'test desc.', self.user) is True
@@ -92,7 +104,7 @@ class Test_Files(object):
             assert len(projects) == 1
 
     def test_is_admin(self):
-        with self.app.app_context():
+        with self.app.test_client():
             assert self.fm.create_project('test_path', 'test desc.', self.user) is True
             p_id = get_recent_pid(self.fm, self.user)
             u_id = self.user.id
@@ -103,10 +115,8 @@ class Test_Files(object):
             assert self.fm.is_admin(u_id, no_perm_p_id) is False
 
     def test_file_save(self):
-        with self.app.app_context():
-            flight_path = "project77"
-            assert self.fm.create_project(flight_path, "something to know", self.user, content="apha")
-            project = Project.query.filter_by(path=flight_path).first()
+        with self.app.test_client():
+            flight_path, project = self._create_project(flight_path="project77")
             assert self.fm.save_file(project.id, "beta", self.user)
             assert self.fm.get_file(project.id, self.user) == "beta"
             assert self.fm.save_file(project.id, "gamma", self.user)
@@ -116,42 +126,34 @@ class Test_Files(object):
             assert len(changes) == 2
 
     def test_undo(self):
-        with self.app.app_context():
-            flight_path = "project7"
-            assert self.fm.create_project(flight_path, "something to know", self.user, content="apha")
-            project = Project.query.filter_by(path=flight_path).first()
-
+        with self.app.test_client():
+            flight_path, project = self._create_project(flight_path="project7", content="alpha")
             assert self.fm.save_file(project.id, "beta", self.user)
             assert self.fm.save_file(project.id, "gamma", self.user)
-
             changes = Change.query.filter_by(p_id=project.id).all()
             assert changes is not None
             assert changes[0].id == 1
             assert self.fm.undo(changes[0].id, self.user) is True
-
             assert len(self.fm.get_all_changes(project.id, self.user)) == 3
             assert "beta" in self.fm.get_file(project.id, self.user)
 
     def test_get_project(self):
-        with self.app.app_context():
-            flight_path = "project9"
-            assert self.fm.create_project(flight_path, "something to know", self.user)
+        with self.app.test_client():
+            self._create_project(flight_path="project9")
             p_id = get_recent_pid(self.fm, self.user)
             assert self.fm.get_file(p_id, self.user) is not False
             user2 = User.query.filter_by(id=9).first()
             assert self.fm.get_file(p_id, user2) is False
 
     def test_authorized_users(self):
-        with self.app.app_context():
-            flight_path = "project10"
-            assert self.fm.create_project(flight_path, "something to know", self.user, content=self.content1)
+        with self.app.test_client():
+            self._create_project(flight_path="project10", content=self.content1)
             p_id = get_recent_pid(self.fm, self.user)
             assert len(self.fm.get_authorized_users(p_id)) == 1
 
     def test_modify_project(self):
-        with self.app.app_context():
-            flight_path = "path"
-            assert self.fm.create_project(flight_path, "something to know", self.user)
+        with self.app.test_client():
+            self._create_project(flight_path="path")
             p_id = get_recent_pid(self.fm, self.user)
             # testing for wrong characters in path like ' ', '/'
             assert self.fm.update_project(p_id, 'path', 'dummy wrong', self.user) is False
@@ -161,9 +163,8 @@ class Test_Files(object):
             assert self.fm.update_project(p_id, 'description', 'dummy', self.user) is True
 
     def test_delete_project(self):
-        with self.app.app_context():
-            flight_path = "f3"
-            assert self.fm.create_project(flight_path, "f3 test example", self.user)
+        with self.app.test_client():
+            self._create_project(flight_path="f3")
             p_id = get_recent_pid(self.fm, self.user)
             assert self.fm.delete_file(p_id, self.user2) is False
             assert self.fm.delete_file(p_id, self.user) is True
@@ -213,3 +214,10 @@ class Test_Files(object):
           </Waypoint>
           </ListOfWaypoints>
       </FlightTrack>"""
+
+    def _create_project(self, flight_path="firstflight", user=None, content=None):
+        if user is None:
+            user = self.user
+        self.fm.create_project(flight_path, f"info about {flight_path}", user, content=content)
+        project = Project.query.filter_by(path=flight_path).first()
+        return flight_path, project
