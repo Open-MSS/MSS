4ca86f4f090a93e16b9a2c27c3ce04273c5c9f98
ReimarBauer <rb.proj@gmail.com>
2022-03-17 09:05:12 +0100

    doc string fixed

--- mslib/mscolab/migrations/env.py
+++ mslib/mscolab/migrations/env.py
@@ -1,3 +1,4 @@
+"""
 The MIT License (MIT)
 
 Copyright (c) 2016 SUNSCRAPERS
@@ -19,8 +20,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
-# https://github.com/sunscrapers/flask-boilerplate/blob/master/migrations/env.py
+"""
+# source https://github.com/sunscrapers/flask-boilerplate/blob/master/migrations/env.py
 
 from __future__ import with_statement
 
