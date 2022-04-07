ac1ce1fc9df61a31282364b9cc67e0a8519193b9
J. Ungermann <j.ungermann@fz-juelich.de>
2022-04-07 13:44:49 +0200

    Moved Target-singleton to use unit-aware ranges and thresholds (#1412)
    Annotating the ranges allows setting them with file-specific units and
    converting them ad-hoc the the configured unit.
    
    Fix #1408

--- mslib/mswms/_tests/test_targets.py
+++ mslib/mswms/_tests/test_targets.py
@@ -31,10 +31,12 @@ from mslib.mswms.utils import Targets
 def test_targets():
     for standard_name in Targets.get_targets():
         unit = Targets.get_unit(standard_name)
-        units(unit[0])  # ensure that the unit may be parsed
-        assert unit[1] == 1  # no conversion outside pint!
+        units(unit)  # ensure that the unit may be parsed
         Targets.get_range(standard_name)
         Targets.get_thresholds(standard_name)
         Targets.get_range(standard_name)
         Targets.UNITS[standard_name]
         Targets.TITLES[standard_name]
+
+    for ent in Targets.THRESHOLDS:
+        units(Targets.THRESHOLDS[ent][0])
