81ba5bfc80555b025d2e7c8474d5c7da083c5197
rootxrishabh <73490663+rootxrishabh@users.noreply.github.com>
2023-02-28 00:39:49 +0530

    added template to test_version.py

--- mslib/_tests/test_version.py
+++ mslib/_tests/test_version.py
@@ -1,11 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    mslib.modulename
+    ~~~~~~~~~~~~~~~~
+    Text line as description
+    This file is part of MSS.
+    :copyright: Copyright 2017 Main Contributor
+    :copyright: Copyright 2016-2022 by the MSS team, see AUTHORS.
+    :license: APACHE-2.0, see LICENSE for details.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+
 import pytest
 from packaging.version import Version
 from mslib import __version__
 
-
-def test_version_string_setuptools():
+#testing import version string for validity
+def test_version_string():
     Version(__version__)
 
     def do_test():
         with pytest.raises():
-            test_version_string_setuptools()
+            test_version_string()
