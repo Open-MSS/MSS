b2c85f79840bf09d06fc3d8f93c9e162ea0af3c5
ReimarBauer <rb.proj@gmail.com>
2023-03-01 11:14:27 +0100

    year 2023 (#1674)

--- mslib/msui/_tests/test_mscolab_version_history.py
+++ mslib/msui/_tests/test_mscolab_version_history.py
@@ -9,7 +9,7 @@
     This file is part of MSS.
 
     :copyright: Copyright 2020 Tanish Grover
-    :copyright: Copyright 2020-2022 by the MSS team, see AUTHORS.
+    :copyright: Copyright 2020-2023 by the MSS team, see AUTHORS.
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
--- mslib/plugins/__init__.py
+++ mslib/plugins/__init__.py
@@ -7,7 +7,7 @@
     This file is part of MSS.
 
     :copyright: Copyright 2017 Joern Ungermann
-    :copyright: Copyright 2017-2022 by the MSS team, see AUTHORS.
+    :copyright: Copyright 2017-2023 by the MSS team, see AUTHORS.
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
