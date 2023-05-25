a08dee9f8c7d8ddae7e85bc16919394abcffc9c9
ReimarBauer <rb.proj@gmail.com>
2023-05-25 16:02:35 +0200

    new test for new migration

--- tests/_test_utils/test_migration.py
+++ tests/_test_utils/test_migration.py
@@ -27,7 +27,7 @@
 import pytest
 import fs
 from packaging import version
-from mslib.utils.migration.update_json_file_to_version_eight import JsonConversion
+
 from mslib.utils import auth
 from mslib.version import __version__
 from mslib.msui.constants import MSUI_SETTINGS
@@ -43,7 +43,8 @@ class TestMigration:
         The test checks on version 8 if an old msui_settings.json can become migrated
         It adds the new attributes and stores passwords in the keyring
         """
-        if version.parse(__version__) >= version.parse('8.0.0'):
+        if version.parse(__version__) >= version.parse('8.0.0') and version.parse(__version__) < version.parse('9.0.0'):
+            from mslib.utils.migration.update_json_file_to_version_eight import JsonConversion
             # old attributes
             wms_def = '"http://www.your-server.de/forecasts": ["youruser", "yourpassword"]'
             msc_def = '"http://www.your-mscolab-server.de": ["youruser", "yourpassword"]'
@@ -101,3 +102,58 @@ class TestMigration:
                 config_loader(dataset="MSC_login")
             with pytest.raises(KeyError):
                 config_loader(dataset="MSCOLAB_password")
+
+    def test_upgrade_json_file_to_version_nine(self):
+        """
+        The test checks on version 8 if an old msui_settings.json can become migrated
+        It adds the new attributes and stores passwords in the keyring
+        """
+        if version.parse(__version__) >= version.parse('9.0.0') and\
+                version.parse(__version__) < version.parse('10.0.0'):
+            from mslib.utils.migration.update_json_file_to_version_nine import JsonConversion
+            # old attributes
+            mailid = 'something@something.org'
+            auth = '"https://www.your-mscolab-server.de": "youruser"'
+            default = '"https://www.your-mscolab-server.de"'
+            data = f"""{{
+            "MSCOLAB_mailid": "{mailid}",
+
+            "MSS_auth": {{
+                             {auth}
+                         }},
+            "default_MSCOLAB": [
+                                   {default}
+                               ]
+            }}"""
+            # store old configuration
+            create_msui_settings_file(data)
+
+            from mslib.utils.migration.config_before_nine import read_config_file as read_config_file_before_nine
+            from mslib.utils.migration.config_before_nine import config_loader as config_loader_before_nine
+            read_config_file_before_nine()
+
+            result = dict()
+            result[default] = mailid
+            assert config_loader_before_nine(dataset="MSS_auth") == {"https://www.your-mscolab-server.de": "youruser"}
+            all = config_loader_before_nine()
+            # old version knows MSCOLAB_mailid
+            assert "MSCOLAB_mailid" in all.keys()
+            new_version = JsonConversion()
+            # converting and storing
+            new_version.change_parameters()
+            filename = MSUI_SETTINGS.replace('\\', '/')
+            dir_name, file_name = fs.path.split(filename)
+            # check that we have a backup file
+            bak_file = f"{file_name}.bak"
+            _fs = fs.open_fs(dir_name)
+            assert _fs.exists(bak_file)
+
+            # using current configuration
+            from mslib.utils.config import read_config_file, config_loader
+            read_config_file()
+            # added MSCOLAB_mailid to the url based on default_MSCOLAB
+            mss_auth = config_loader(dataset="MSS_auth")
+            assert mss_auth == {"https://www.your-mscolab-server.de": mailid}
+            all = config_loader()
+            # new version forgot about MSCOLAB_mailid
+            assert "MSCOLAB_mailid" not in all.keys()
