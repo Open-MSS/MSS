5e7d816b82fa149bbb061ed2d8ae2cbb539bc3da
nilupulmanodya <n.manodya@gmail.com>
2023-07-25 21:29:36 +0530

    update docstring

--- mslib/msui/msui_web_browser.py
+++ mslib/msui/msui_web_browser.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 """
+
     mslib.msui.msui_web_browser.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    QTWebBrowser for the SSO process.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    MSUIWebBrowser can be used for localhost usage and testing purposes.
+
     This file is part of MSS.
+
     :copyright: Copyright 2023 Nilupul Manodya
     :license: APACHE-2.0, see LICENSE for details.
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
