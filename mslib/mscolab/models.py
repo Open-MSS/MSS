433aeabbdae5b5e93296332dead2b59ca7282e2d
nilupulmanodya <n.manodya@gmail.com>
2023-08-01 03:10:01 +0530

    update db models

--- mslib/mscolab/models.py
+++ mslib/mscolab/models.py
@@ -48,7 +48,7 @@ class User(db.Model):
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
     permissions = db.relationship('Permission', cascade='all,delete,delete-orphan', backref='user')
-    authentication_backend = db.Column(db.String(255), nullable=True, default=False)
+    authentication_backend = db.Column(db.String(255), nullable=False, default='local')
 
     def __init__(self, emailid, username, password, confirmed=False, confirmed_on=None, authentication_backend='local'):
         self.username = username
