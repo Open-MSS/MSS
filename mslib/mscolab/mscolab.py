0c83eff1fa2f9db0b050d43f8ebb518408cd1672
Matthias Riße <m.risse@fz-juelich.de>
2023-11-07 16:05:01 +0100

    Replace SIGKILL with SIGTERM
    SIGTERM is the less invasive one, allowing the process to gracefully
    shutdown on its own.

--- mslib/mscolab/mscolab.py
+++ mslib/mscolab/mscolab.py
@@ -280,7 +280,7 @@ def handle_mscolab_metadata_init(repo_exists):
         cmd_curl = ["curl", "http://localhost:8083/metadata/localhost_test_idp",
                     "-o", f"{mscolab_settings.MSCOLAB_SSO_DIR}/metadata_sp.xml"]
         subprocess.run(cmd_curl, check=True)
-        process.kill()
+        process.terminate()
         print('mscolab metadata file generated succesfully')
         return True
 
