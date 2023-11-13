e34bf02932dad4833b5ec53406114615c9b3ac14
ReimarBauer <rb.proj@gmail.com>
2023-11-13 11:15:07 +0100

    typo

--- tests/_test_msui/test_mss.py
+++ tests/_test_msui/test_mss.py
@@ -30,7 +30,7 @@ from PyQt5 import QtWidgets, QtTest, QtCore
 from mslib.msui import mss
 
 
-@pytest.mark.skip(reason=i'needs review, assert missing')
+@pytest.mark.skip(reason='needs review, assert missing')
 def test_mss_rename_message():
     application = QtWidgets.QApplication(sys.argv)
     main_window = mss.MSSMainWindow()
