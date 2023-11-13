f5d1edf5303edddec646e4042e750b057082fdfe
ReimarBauer <rb.proj@gmail.com>
2023-11-13 11:11:20 +0100

    skip test

--- tests/_test_msui/test_mss.py
+++ tests/_test_msui/test_mss.py
@@ -24,12 +24,13 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-
+import pytest
 import sys
 from PyQt5 import QtWidgets, QtTest, QtCore
 from mslib.msui import mss
 
 
+@pytest.mark.skip(reason=i'needs review, assert missing')
 def test_mss_rename_message():
     application = QtWidgets.QApplication(sys.argv)
     main_window = mss.MSSMainWindow()
@@ -38,3 +39,4 @@ def test_mss_rename_message():
     QtWidgets.QApplication.processEvents()
     application.quit()
     QtWidgets.QApplication.processEvents()
+
