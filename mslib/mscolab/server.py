aa986a68a7806bffcea924dae8081b74ff51253a
ReimarBauer <rb.proj@gmail.com>
2023-12-06 11:26:15 +0100

    verify that the user object is not a boolean, in this case not False

--- mslib/mscolab/server.py
+++ mslib/mscolab/server.py
@@ -260,7 +260,7 @@ def get_auth_token():
     emailid = request.form['email']
     password = request.form['password']
     user = check_login(emailid, password)
-    if user:
+    if user is not False:
         if mscolab_settings.MAIL_ENABLED:
             if user.confirmed:
                 token = user.generate_auth_token()
