79c5c4e37f296cfeae699831756beb6ed9c23556
Preetam-Das26 <daspreetam4@gmail.com>
2024-02-16 18:08:34 +0530

    16/02/2024 18:06

--- mslib/mscolab/models.py
+++ mslib/mscolab/models.py
@@ -42,7 +42,7 @@ class User(db.Model):
     password = db.Column(db.String(255), unique=True)
     registered_on = db.Column(db.DateTime, nullable=False)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
-    confirmed_on = db.Column(TIMESTAMP(timezone=True), nullable=True, default=None)
+    confirmed_on = db.Column(db.TIMESTAMP(timezone=True), nullable=True, default=None)
     permissions = db.relationship('Permission', cascade='all,delete,delete-orphan', backref='user')
     authentication_backend = db.Column(db.String(255), nullable=False, default='local')
 
