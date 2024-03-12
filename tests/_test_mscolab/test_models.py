7f50b59d7f8295d4b95964d7eb829f7cf718f1a4
Preetam-Das26 <daspreetam4@gmail.com>
2024-03-12 20:46:21 +0530

    remove class User 20:46

--- tests/_test_mscolab/test_models.py
+++ tests/_test_mscolab/test_models.py
@@ -33,97 +33,109 @@ from mslib.mscolab.server import register_user
 from mslib.mscolab.models import AwareDateTime, User, Permission, Operation, Message, Change
 
 
-class Test_User:
-    @pytest.fixture(autouse=True)
-    def setup(self, mscolab_app):
-        self.userdata = 'UV10@uv10', 'UV10', 'uv10'
-        with mscolab_app.app_context():
-            result = register_user(self.userdata[0], self.userdata[1], self.userdata[2])
-            assert result["success"] is True
-            yield
+@pytest.fixture(autouse=True)
+def setup(mscolab_app):
+    userdata = 'UV10@uv10', 'UV10', 'uv10'
+    with mscolab_app.app_context():
+        result = register_user(userdata[0], userdata[1], userdata[2])
+        assert result["success"] is True
+        yield
 
-    def test_generate_auth_token(self):
-        user = User(self.userdata[0], self.userdata[1], self.userdata[2])
-        token = user.generate_auth_token()
-        assert token is not None
-        assert len(token) > 20
 
-    def test_verify_auth_token(self):
-        user = User(self.userdata[0], self.userdata[1], self.userdata[2])
-        token = user.generate_auth_token()
-        uid = user.verify_auth_token(token)
-        assert user.id == uid
+def test_generate_auth_token():
+    userdata = 'UV10@uv10', 'UV10', 'uv10'
+    user = User(userdata[0], userdata[1], userdata[2])
+    token = user.generate_auth_token()
+    assert token is not None
+    assert len(token) > 20
 
-    def test_verify_password(self):
-        user = User(self.userdata[0], self.userdata[1], self.userdata[2])
-        assert user.verify_password("fail") is False
-        assert user.verify_password(self.userdata[2]) is True
 
-    def test_aware_datetime_conversion(self):
-        aware_datetime_type = AwareDateTime()
-        curr_time = datetime.datetime.now(tz=datetime.timezone.utc)
+def test_verify_auth_token():
+    userdata = 'UV10@uv10', 'UV10', 'uv10'
+    user = User(userdata[0], userdata[1], userdata[2])
+    token = user.generate_auth_token()
+    uid = user.verify_auth_token(token)
+    assert user.id == uid
 
-        result_bind = aware_datetime_type.process_bind_param(curr_time, None)
-        assert result_bind is not None
-        assert result_bind == curr_time
 
-        result_result = aware_datetime_type.process_result_value(curr_time, None)
-        assert result_result is not None
-        assert result_result == curr_time
+def test_verify_password():
+    userdata = 'UV10@uv10', 'UV10', 'uv10'
+    user = User(userdata[0], userdata[1], userdata[2])
+    assert user.verify_password("fail") is False
+    assert user.verify_password(userdata[2]) is True
 
-        result_none = aware_datetime_type.process_bind_param(None, None)
-        assert result_none is None
 
-        cet_time = datetime.datetime.now(tz=pytz.timezone("CET"))
-        result_cet = aware_datetime_type.process_bind_param(cet_time, None)
-        assert result_cet == cet_time
-        assert result_cet is not None
+def test_aware_datetime_conversion():
+    aware_datetime_type = AwareDateTime()
+    curr_time = datetime.datetime.now(tz=datetime.timezone.utc)
 
-    def test_permission_creation(self):
-        permission = Permission(1, 1, "admin")
+    result_bind = aware_datetime_type.process_bind_param(curr_time, None)
+    assert result_bind is not None
+    assert result_bind == curr_time
 
-        assert permission.u_id == 1
-        assert permission.op_id == 1
-        assert permission.access_level == "admin"
+    result_result = aware_datetime_type.process_result_value(curr_time, None)
+    assert result_result is not None
+    assert result_result == curr_time
 
-    @pytest.mark.parametrize("access_level", ["collaborator", "viewer", "creator"])
-    def test_permission_repr_values(self, access_level):
-        permission = Permission(1, 1, access_level)
-        expected_repr = textwrap.dedent(f'''\
-            <Permission u_id: {1}, op_id:{1}, access_level: {access_level}>''').strip()
+    result_none = aware_datetime_type.process_bind_param(None, None)
+    assert result_none is None
 
-        assert repr(permission).strip() == expected_repr
+    cet_time = datetime.datetime.now(tz=pytz.timezone("CET"))
+    result_cet = aware_datetime_type.process_bind_param(cet_time, None)
+    assert result_cet == cet_time
+    assert result_cet is not None
 
-    def test_operation_creation(self):
-        operation = Operation("/path/to/operation", "Description of the operation", category="test_category")
 
-        assert operation.path == "/path/to/operation"
-        assert operation.description == "Description of the operation"
-        assert operation.category == "test_category"
-        assert operation.active is True
+def test_permission_creation():
+    permission = Permission(1, 1, "admin")
 
-    def test_repr(self):
-        operation = Operation("/path/to/operation", "Description of the operation", category="test_category")
-        expected_repr = f'<Operation path: {"/path/to/operation"}, desc: {"Description of the operation"},' \
-                        f' cat: {"test_category"}, active: True, ' \
-                        f'last_used: {operation.last_used}> '
+    assert permission.u_id == 1
+    assert permission.op_id == 1
+    assert permission.access_level == "admin"
 
-        assert repr(operation) == expected_repr
 
-    def test_message_creation(self):
-        message = Message(1, 1, "Hello, this is a test message", "TEXT", None)
+@pytest.mark.parametrize("access_level", ["collaborator", "viewer", "creator"])
+def test_permission_repr_values(access_level):
+    permission = Permission(1, 1, access_level)
+    expected_repr = textwrap.dedent(f'''\
+        <Permission u_id: {1}, op_id:{1}, access_level: {access_level}>''').strip()
 
-        assert message.op_id == 1
-        assert message.u_id == 1
-        assert message.text == "Hello, this is a test message"
-        assert message.message_type == "TEXT"
-        assert message.reply_id is None
+    assert repr(permission).strip() == expected_repr
 
-    def test_change_creation(self):
-        change = Change(1, 1, "#abcdef123456", "v1.0", "Initial commit")
 
-        assert change.op_id == 1
-        assert change.u_id == 1
-        assert change.commit_hash == "#abcdef123456"
-        assert change.version_name == "v1.0"
-        assert change.comment == "Initial commit"
+def test_operation_creation():
+    operation = Operation("/path/to/operation", "Description of the operation", category="test_category")
+
+    assert operation.path == "/path/to/operation"
+    assert operation.description == "Description of the operation"
+    assert operation.category == "test_category"
+    assert operation.active is True
+
+
+def test_operation_repr():
+    operation = Operation("/path/to/operation", "Description of the operation", category="test_category")
+    expected_repr = f'<Operation path: {"/path/to/operation"}, desc: {"Description of the operation"},' \
+                    f' cat: {"test_category"}, active: True, ' \
+                    f'last_used: {operation.last_used}> '
+
+    assert repr(operation) == expected_repr
+
+
+def test_message_creation():
+    message = Message(1, 1, "Hello, this is a test message", "TEXT", None)
+
+    assert message.op_id == 1
+    assert message.u_id == 1
+    assert message.text == "Hello, this is a test message"
+    assert message.message_type == "TEXT"
+    assert message.reply_id is None
+
+
+def test_change_creation():
+    change = Change(1, 1, "#abcdef123456", "v1.0", "Initial commit")
+
+    assert change.op_id == 1
+    assert change.u_id == 1
+    assert change.commit_hash == "#abcdef123456"
+    assert change.version_name == "v1.0"
+    assert change.comment == "Initial commit"
