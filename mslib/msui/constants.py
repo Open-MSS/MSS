90e276b8dd470dfd6b8bbc84e14b3e92bca540a8
Preetam-Das26 <daspreetam4@gmail.com>
2024-03-19 02:56:37 +0530

    Exception scope 02:56

--- mslib/msui/constants.py
+++ mslib/msui/constants.py
@@ -37,10 +37,8 @@ MSUI_CONFIG_PATH = os.getenv("MSUI_CONFIG_PATH", os.path.join(HOME, ".config", "
 _fs = None
 if '://' in MSUI_CONFIG_PATH:
     try:
-        _fs = fs.open_fs(MSUI_CONFIG_PATH)
         _fs = fs.open_fs(fs.path.dirname(MSUI_CONFIG_PATH))
     except fs.errors.CreateFailed:
-        _fs.makedirs(MSUI_CONFIG_PATH)
         _fs.makedirs(fs.path.basename(MSUI_CONFIG_PATH))
     except fs.opener.errors.UnsupportedProtocol:
         logging.error('FS url "%s" not supported', MSUI_CONFIG_PATH)
