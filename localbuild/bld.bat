fae2e5db8b26e014392ce96635ed410003e5a19b
Reimar Bauer <rb.proj@gmail.com>
2024-03-21 13:44:16 +0100

    update

--- localbuild/bld.bat
+++ localbuild/bld.bat
@@ -1,4 +1,7 @@
 mkdir "%PREFIX%\Menu"
 copy /Y "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
+copy /Y "%RECIPE_DIR%\msui.ico" "%PREFIX%\Menu\msui.ico"
+
+
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
