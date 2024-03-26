52b0cdf80f79f03516e4c093e4ed8e381ec48b92
Matthias Riße <m.risse@fz-juelich.de>
2024-03-26 10:23:05 +0100

    CI overhaul
    This removes the docker image logic from the CI setup and instead
    replaces it with a setup based on pixi. As part of that it moves the
    dependency specification from localbuild/meta.yaml to pixi.toml and
    pixi.lock. This turns the MSS repository into a single source of truth
    for both the application code as well as the development environment
    (whereas the latter was previously only specified in the docker images,
    and not reproducible in any way).
    
    Setting up a development environment is as simple as installing pixi and
    running `pixi shell` (or `pixi run <cmd>`, or `pixi install` to just
    create the environment, etc.). This environment will, by construction,
    be the same that is used in the CI as well (modulo platform
    differences).
    
    There is a new workflow that periodically (once a week on Monday)
    recreates the pixi lockfile and opens a PR for that update. The checks
    in that PR essentially serve as a replacement for the previous scheduled
    runs to ensure that no dependency update breaks MSS. Merging that PR is
    a manual step that can be done just as with any other PR and would then
    update the environment on the given target branch. This is essentially
    what was previously the triggering of a docker image creation.
    
    Including new dependencies can be done with `pixi add`, which will also
    automatically add the dependency to the (pre-existing) lockfile. This
    means dependency additions can be part of the PR that necessitate them
    and they won't affect the entire environment (as they previously did,
    where they would trigger a full image rebuild) but instead just add that
    new package to the existing specification.

--- localbuild/bld.bat
+++
@@ -1,9 +0,0 @@
-set MENU_DIR=%PREFIX%\Menu
-if not exist %MENU_DIR% mkdir %MENU_DIR%
-if errorlevel 1 exit 1
-copy %SRC_DIR%\Menu\msui_shortcut.json %MENU_DIR%\msui_shortcut.json
-if errorlevel 1 exit 1
-copy %SRC_DIR%\Menu\mss.ico %MENU_DIR%\mss.ico
-if errorlevel 1 exit 1
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
