e618b836e6bc796310608944345c570fd203d3dc
ReimarBauer <rb.proj@gmail.com>
2024-05-06 15:10:38 +0200

    removed python pinning
    oldstyle build for windows

--- localbuild/bld.bat
+++ localbuild/bld.bat
@@ -2,6 +2,6 @@ mkdir "%PREFIX%\Menu"
 copy /Y "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
 copy /Y "%RECIPE_DIR%\msui.ico" "%PREFIX%\Menu\msui.ico"
 
-
-%PYTHON% -m pip install . --no-deps -vv
+; conda-build issue 5311, currently we can't build for windows with pip install
+%PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
