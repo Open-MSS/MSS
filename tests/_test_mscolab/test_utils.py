3d41c17a2106821d8925e610f018eb31ff4e6df4
ReimarBauer <rb.proj@gmail.com>
2024-05-14 14:30:25 +0200

    move MessageType check into a test where we can generate a message

--- tests/_test_mscolab/test_utils.py
+++ tests/_test_mscolab/test_utils.py
@@ -29,28 +29,9 @@ import json
 
 from fs.tempfs import TempFS
 from mslib.mscolab.conf import mscolab_settings
-from mslib.mscolab.models import Operation, MessageType
+from mslib.mscolab.models import Operation
 from mslib.mscolab.seed import add_user, get_user
-from mslib.mscolab.utils import (get_recent_op_id, get_session_id,
-                                 get_message_dict, create_files,
-                                 os_fs_create_dir)
-
-
-class Message:
-    id = 1  # noqa: A003
-    u_id = 2
-
-    class user:
-        username = "name"
-    text = "Moin"
-    message_type = MessageType.TEXT
-    reply_id = 0
-    replies = []
-
-    class created_at:
-        @staticmethod
-        def isoformat():
-            pass
+from mslib.mscolab.utils import get_recent_op_id, get_session_id, create_files, os_fs_create_dir
 
 
 class Test_Utils:
@@ -78,10 +59,6 @@ class Test_Utils:
     def test_get_session_id(self):
         sockets = [{"u_id": 5, "s_id": 100}]
         assert get_session_id(sockets, 5) == 100
-
-    def test_get_message_dict(self):
-        result = get_message_dict(Message())
-        assert result["message_type"] == MessageType.TEXT
 
     def test_os_fs_create_dir(self):
         _fs = TempFS(identifier="msui")
