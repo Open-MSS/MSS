d44f55a3d0024c179c4214eaa595eeecbbdf7bc7
Matthias Riße <m.risse@fz-juelich.de>
2024-08-16 14:58:39 +0200

    Fix sequences after migrating data to PostgreSQL
    When inserting rows into PostgreSQL tables and explicitly setting values
    for e.g. auto-increment columns, like the IDs, the associated sequences
    aren't automatically updated. This means that without explicitly fixing
    the sequences PostgreSQL would try to insert new rows with existing
    values for the IDs, failing in the process due to unique constraints.
    
    This issue doesn't affect MariaDB nor SQLite. The former automatically
    updates auto-increment counters on insert, the latter simply uses the
    last rows ID as a starting point.
    
    This change also extends the tests to try adding a new user after the
    migration, which should catch this kind of issue.

--- mslib/mscolab/server.py
+++ mslib/mscolab/server.py
@@ -110,6 +110,34 @@ your database MSColab will abort. Please follow the documentation for a manual d
                     )
                     target_connection.execute(stmt.values(row))
             target_connection.commit()
+            if target_engine.name == "postgresql":
+                # Fix the databases auto-increment sequences, if it is a PostgreSQL database
+                # For reference, see: https://wiki.postgresql.org/wiki/Fixing_Sequences
+                logging.info("Using a PostgreSQL database, will fix up sequences")
+                cur = target_connection.execute(sqlalchemy.text(r"""
+SELECT
+    'SELECT SETVAL(' ||
+    quote_literal(quote_ident(sequence_namespace.nspname) || '.' || quote_ident(class_sequence.relname)) ||
+    ', COALESCE(MAX(' ||quote_ident(pg_attribute.attname)|| '), 1) ) FROM ' ||
+    quote_ident(table_namespace.nspname)|| '.'||quote_ident(class_table.relname)|| ';'
+FROM pg_depend
+    INNER JOIN pg_class AS class_sequence
+        ON class_sequence.oid = pg_depend.objid
+            AND class_sequence.relkind = 'S'
+    INNER JOIN pg_class AS class_table
+        ON class_table.oid = pg_depend.refobjid
+    INNER JOIN pg_attribute
+        ON pg_attribute.attrelid = class_table.oid
+            AND pg_depend.refobjsubid = pg_attribute.attnum
+    INNER JOIN pg_namespace as table_namespace
+        ON table_namespace.oid = class_table.relnamespace
+    INNER JOIN pg_namespace AS sequence_namespace
+        ON sequence_namespace.oid = class_sequence.relnamespace
+ORDER BY sequence_namespace.nspname, class_sequence.relname;
+"""))
+                for stmt, in cur.all():
+                    target_connection.execute(sqlalchemy.text(stmt))
+                target_connection.commit()
         logging.info("Data migration finished")
 
     # Upgrade to the latest database revision
