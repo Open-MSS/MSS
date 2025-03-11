11e5fc74a2bcefe66bc27a692cbb1796e6ea113c
Joern Ungermann <j.ungermann@fz-juelich.de>
2025-03-11 12:05:45 +0100

    added ValueError to list of KML exceptions

--- mslib/msui/kmloverlay_dockwidget.py
+++ mslib/msui/kmloverlay_dockwidget.py
@@ -567,7 +567,7 @@ class KMLOverlayControlWidget(QtWidgets.QWidget, ui.Ui_KMLOverlayDockWidget):
                                                  self.dict_files[self.listWidget.item(index).text()]["linewidth"])
                             self.dict_files[self.listWidget.item(index).text()]["patch"] = patch
 
-                except (AttributeError, IOError, TypeError, et.XMLSyntaxError, et.XMLSchemaError,
+                except (AttributeError, IOError, TypeError, ValueError, et.XMLSyntaxError, et.XMLSchemaError,
                         et.XMLSchemaParseError, et.XMLSchemaValidateError) as ex:  # catches KML Syntax Errors
                     logging.error("KML Overlay - %s: %s", type(ex), ex)
                     self.labelStatusBar.setText(str(self.listWidget.item(index).text()) +
