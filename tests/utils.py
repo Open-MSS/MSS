059c8d7a179f90f7e37559eedbc62ddbb82ca77e
Reimar Bauer <rb.proj@gmail.com>
2025-03-28 16:28:25 +0100

    try 0 seconds

--- tests/utils.py
+++ tests/utils.py
@@ -221,7 +221,7 @@ def create_msui_settings_file(content):
 
 
 def is_url_response_ok(url):
-    time.sleep(1)  # it seems we need a delay?
+    time.sleep(0)  # it seems we need a delay?
     try:
         response = requests.get(url, timeout=2)
         return response.status_code == 200
