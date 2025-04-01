644e55e423fa9ccd5e2e7526de82fbacd8530ca1
Anshuman Panda <ichuk0078@gmail.com>
2025-04-01 21:22:31 +0000

    Fix to super so that it is properly called to initialize the parent class netCDF4.MFDataset.

--- mslib/utils/netCDF4tools.py
+++ mslib/utils/netCDF4tools.py
@@ -200,7 +200,8 @@ class MFDatasetCommonDims(netCDF4.MFDataset):
 
     def __init__(self, files, exclude=None, skip_dim_check=None,
                  require_dim_num=False):
-        super().__init__(files)
+        super().__init__(files, exclude=exclude or [],
+                         require_dim_num=require_dim_num)
         """
         Open a Dataset spanning multiple files sharing common dimensions but
         containing different record variables, making it look as if it was a
