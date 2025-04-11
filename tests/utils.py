16c0844c494715bf19489a95868d571792676bda
Reimar Bauer <rb.proj@gmail.com>
2025-04-11 09:41:42 +0200

    removed workaround timeout

--- tests/utils.py
+++ tests/utils.py
@@ -221,7 +221,6 @@ def create_msui_settings_file(content):
 
 
 def is_url_response_ok(url):
-    time.sleep(0.5)  # workaround until we know the reason for needing that
     try:
         response = requests.get(url, timeout=2)
         return response.status_code == 200
