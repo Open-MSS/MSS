d826c0d6a04a736416a2046bc12e7053897724cf
Reimar Bauer <rb.proj@gmail.com>
2025-07-04 16:51:20 +0200

    geometry from pygeoif

--- mslib/msui/kmloverlay_dockwidget.py
+++ mslib/msui/kmloverlay_dockwidget.py
@@ -27,9 +27,9 @@
 import copy
 import logging
 from fastkml import kml
-from fastkml.geometry import (Point, LineString, LinearRing, Polygon, MultiLineString,
-                              MultiPolygon, GeometryCollection)
-from pygeoif import MultiPoint
+from pygeoif.geometry import (Point, LineString, LinearRing, Polygon,
+                              MultiPoint, MultiLineString, MultiPolygon)
+
 from fastkml.styles import Style, LineStyle, PolyStyle
 from lxml import etree as et, objectify
 import os
