29840ccd782731353a7162a41ab71e655c821f3f
Matthias Riße <m.risse@fz-juelich.de>
2025-08-25 10:16:58 +0200

    Replace setup.py with pyproject.toml
    This eliminates the need for the future module by setting the mslib
    version in pyproject.toml and populating __version__ from package
    metadata. One limitation of pyproject.toml is that it is not possible to
    define OS-specific entrypoints, so the msidp application now has a
    runtime check to error out on Windows instead of just not existing
    there.
    
    The pyproject.toml file is the latest PEP standard for python project
    metadata. In the future it could also accomodate additional
    configuration that are currently set in setup.cfg and pytest.ini.

--- mslib/msidp/idp.py
+++ mslib/msidp/idp.py
@@ -1092,6 +1092,9 @@ def main():
         '\033[91mWARNING: msidp is solely for development and '
         'testing purposes; do not use in production environments.\033[0m'
     )
+    if os.name == "nt":
+        logger.error("The msidp application is not usable on Windows.")
+        return 1
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", dest="path", help="Path to configuration file.",
                         default="./idp_conf.py")
