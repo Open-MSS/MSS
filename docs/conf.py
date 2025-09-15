fdfe1d1ff8a2dad88f9b502ee9dd59340cdf88be
Reimar Bauer <rb.proj@gmail.com>
2025-09-15 21:22:35 +0200

    use binary mode

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ if os.environ.get("GALLERY", "True") != "False":
     mslib.mswms.wms.server.generate_gallery(sphinx=True, generate_code=True, all_plots=True, levels="3,4,200,300",
                                             vtimes="2012-10-18T00:00:00,2012-10-19T00:00:00")
 
-with open("../pyproject.toml") as f:
+with open("../pyproject.toml", "rb") as f:
     __version__ = tomllib.load(f)["project"]["version"]
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
