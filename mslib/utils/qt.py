194d18b843b0a471136c3d36d27d0b776338093a
Reimar Bauer <rb.proj@gmail.com>
2026-01-14 17:02:38 +0100

    y2026

--- mslib/utils/qt.py
+++ mslib/utils/qt.py
@@ -9,7 +9,7 @@
     This file is part of MSS.
 
     :copyright: Copyright 2017-2018 Joern Ungermann, Reimar Bauer
-    :copyright: Copyright 2017-2025 by the MSS team, see AUTHORS.
+    :copyright: Copyright 2017-2026 by the MSS team, see AUTHORS.
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
