3203c47569146123db49d53898755fe951d91b82
Reimar Bauer <rb.proj@gmail.com>
2026-01-26 20:29:44 +0100

    update

--- mslib/msui/flighttrack.py
+++ mslib/msui/flighttrack.py
@@ -635,6 +635,10 @@ class WaypointsTableModel(QtCore.QAbstractTableModel):
         doc = self.get_xml_doc()
         with open(filename, "w") as file_object:
             doc.writexml(file_object, indent="  ", addindent="  ", newl="\n", encoding="utf-8")
+            # the with should ensure that the file is flushed and synced to disk
+            # nevertheless we try if that changes the linux test behavior
+            file_object.flush()
+            os.fsync(file_object.fileno())
         self.name = os.path.basename(self.filename).replace(".ftml", "").strip()
 
     def get_xml_doc(self):
