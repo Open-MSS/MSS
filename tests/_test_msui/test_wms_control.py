cd6928fd1811cfbc14f7011e9efae1f59cb8afe5
Reimar Bauer <rb.proj@gmail.com>
2026-03-30 18:15:53 +0200

    use a fixture to always clear _shared_cache

--- tests/_test_msui/test_wms_control.py
+++ tests/_test_msui/test_wms_control.py
@@ -33,7 +33,6 @@ import urllib
 import eventlet
 from PyQt5 import QtCore, QtTest
 from mslib.msui import flighttrack as ft
-from mslib.utils.service_manager import WMSServiceManager
 import mslib.msui.wms_control as wc
 
 
@@ -57,8 +56,6 @@ class WMSControlWidgetSetup:
         self.scheme, self.host, self.port = parsed_url.scheme, parsed_url.hostname, parsed_url.port
 
     def _setup(self, widget_type, tmp_path):
-        self.service_manager = WMSServiceManager()
-        self.service_manager.clear_cache()
         if widget_type == "hsec":
             self.view = HSecViewMockup()
         else:
@@ -500,8 +497,6 @@ class TestWMSControlWidgetSetupSimple:
 
     @pytest.fixture(autouse=True)
     def setup(self, tmp_path, qtbot):
-        self.service_manager = WMSServiceManager()
-        self.service_manager.clear_cache()
         self.view = HSecViewMockup()
         self.tempdir = tmp_path
         self.window = wc.HSecWMSControlWidget(view=self.view, wms_cache=self.tempdir)
