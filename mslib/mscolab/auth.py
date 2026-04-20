dc2507e0674871bcea22df516cd7b2c44ab1364e
J.Ironman <j.eisermann@fz-juelich.de>
2026-04-20 16:20:56 +0200

    added description header

--- mslib/mscolab/auth.py
+++ mslib/mscolab/auth.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+
+    mslib.utils.auth
+    ~~~~~~~~~~~~~~~~
+
+    handles passwords from the keyring for login and http_auuth
+
+
+    To better understand of the code, look at the 'ships' example from
+    chapter 14/16 of 'Rapid GUI Programming with Python and Qt: The
+    Definitive Guide to PyQt Programming' (Mark Summerfield).
+
+    This file is part of MSS.
+
+    :copyright: Copyright 2023 Reimar Bauer
+    :copyright: Copyright 2023-2026 by the MSS team, see AUTHORS.
+    :license: APACHE-2.0, see LICENSE for details.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+
 import datetime
 import functools
 import logging
