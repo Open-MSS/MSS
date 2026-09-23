b554a8327f7950084edc7107b98a23a070b9b0fb
J.Ironman <j.eisermann@fz-juelich.de>
2026-09-23 11:37:56 +0200

    corrected copyright

--- tests/_test_msui/test_autoplot_dockwidget.py
+++ tests/_test_msui/test_autoplot_dockwidget.py
@@ -8,7 +8,7 @@
 
     This file is part of MSS.
 
-    :copyright: Copyright 2026 by the MSS team, see AUTHORS.
+    :copyright: Copyright 2026 Jan Eisermann
     :license: APACHE-2.0, see LICENSE for details.
 
     Licensed under the Apache License, Version 2.0 (the "License");
